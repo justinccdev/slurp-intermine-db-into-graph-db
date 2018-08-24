e79552a113cf89ab4c6898d357a9cd8b495ff42b
Justin Clark-Casey <justincc@justincc.org>
2018-08-24 13:02:03 +0100

    very basic start to get one the id and class of one gene row in synbiomine-v6 and display

--- slurp.py
+++ slurp.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
-import psycopg2
+import psycopg2.extras
 
+conn = psycopg2.connect(dbname='synbiomine-v6', user='justincc', cursor_factory=psycopg2.extras.DictCursor)
+
+with conn.cursor() as curs:
+    curs.execute("SELECT primaryidentifier, class FROM gene WHERE primaryIdentifier='b0001'")
+    row = curs.fetchone()
+    print(row['primaryidentifier'], row['class'])
+
+conn.close()
