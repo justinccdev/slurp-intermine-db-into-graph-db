aee1f47a4739218d203fd92e00e4e0c5f1fa6213
Justin Clark-Casey <justincc@justincc.org>
2018-08-24 17:09:03 +0100

    create a single gene node in neo4j

--- slurp.py
+++ slurp.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
+import neo4j.v1
 import psycopg2.extras
 
 conn = psycopg2.connect(dbname='synbiomine-v6', user='justincc', cursor_factory=psycopg2.extras.DictCursor)
 
-with conn.cursor() as curs:
-    curs.execute("SELECT primaryidentifier, class FROM gene WHERE primaryIdentifier='b0001'")
-    row = curs.fetchone()
-    print(row['primaryidentifier'], row['class'])
+with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'passw0rd')) as driver:
+    with driver.session() as session:
+        with conn.cursor() as curs:
+            curs.execute("SELECT primaryidentifier, class FROM gene WHERE primaryIdentifier='b0001'")
+
+            for row in curs:
+                id, type = row['primaryidentifier'], row['class']
+                print(id, type)
+                session.run("CREATE (:gene { id:'%s', type:'%s' })" % (id, type))
 
 conn.close()
