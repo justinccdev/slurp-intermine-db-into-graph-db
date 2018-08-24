d3d19f85dba5d96d2feef06c6f6bfe570d2e7af4
Justin Clark-Casey <justincc@justincc.org>
2018-08-24 18:17:34 +0100

    link genes to organism

--- slurp.py
+++ slurp.py
@@ -15,19 +15,26 @@ with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'pass
             curs.execute("SELECT id, primaryidentifier, organismid, class FROM gene LIMIT 5")
 
             for row in curs:
-                genes[row['id']] = { 'external_primary_id': row['primaryidentifier'], 'type' : row['class'] }
+                genes[row['id']] = {
+                    'external_primary_id': row['primaryidentifier'],
+                    'internal_organism_id': row['organismid'],
+                    'type' : row['class']
+                }
 
-            for gene in genes.values():
+            for im_id, gene in genes.items():
                 print(gene)
-                session.run("CREATE (:gene { id:'%s', type:'%s' })" % (gene['external_primary_id'], gene['type']))
+                session.run("CREATE (:gene { im_id:'%s', id:'%s', type:'%s' })" % (im_id, gene['external_primary_id'], gene['type']))
 
             curs.execute("SELECT id, taxonid, name, class FROM organism")
 
             for row in curs:
                 organisms[row['id']] = { 'external_primary_id': row['taxonid'], 'name': row['name'], 'type': row['class'] }
 
-            for organism in organisms.values():
+            for im_id, organism in organisms.items():
                 print(organism)
-                session.run("CREATE (:organism { id:'%s', name: '%s', type:'%s' })" % (id, name, type))
+                session.run("CREATE (:organism { im_id:'%s', id:'%s', name: '%s', type:'%s' })" % (im_id, organism['external_primary_id'], organism['name'], organism['type']))
+
+            for im_id, gene in genes.items():
+                session.run("MATCH (g:gene {im_id:'%s'}), (o:organism {im_id:'%s'}) CREATE (g)-[:IN_GENOME_OF]->(o)" % (im_id, gene['internal_organism_id']))
 
 conn.close()
