87fe3d0e95773d5102c9bc3b26ba9419ffcfbb84
Justin Clark-Casey <justincc@justincc.org>
2018-08-24 17:37:49 +0100

    put genes and organism into in-memory structures

--- slurp.py
+++ slurp.py
@@ -3,23 +3,31 @@
 import neo4j.v1
 import psycopg2.extras
 
+
+genes = {}
+organisms = {}
+
 conn = psycopg2.connect(dbname='synbiomine-v6', user='justincc', cursor_factory=psycopg2.extras.DictCursor)
 
 with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'passw0rd')) as driver:
     with driver.session() as session:
         with conn.cursor() as curs:
-            curs.execute("SELECT primaryidentifier, class FROM gene")
+            curs.execute("SELECT id, primaryidentifier, class FROM gene")
 
             for row in curs:
-                id, type = row
-                print(id, type)
-                session.run("CREATE (:gene { id:'%s', type:'%s' })" % (id, type))
+                genes[row['id']] = { 'external_primary_id': row['primaryidentifier'], 'type' : row['class'] }
 
-            curs.execute("SELECT taxonid, name, class FROM organism")
+            for gene in genes.values():
+                print(gene)
+                session.run("CREATE (:gene { id:'%s', type:'%s' })" % (gene['external_primary_id'], gene['type']))
+
+            curs.execute("SELECT id, taxonid, name, class FROM organism")
 
             for row in curs:
-                id, name, type = row
-                print(id, name, type)
+                organisms[row['id']] = { 'external_primary_id': row['taxonid'], 'name': row['name'], 'type': row['class'] }
+
+            for organism in organisms.values():
+                print(organism)
                 session.run("CREATE (:organism { id:'%s', name: '%s', type:'%s' })" % (id, name, type))
 
 conn.close()
