f34af2a0727db2750d547d71bf8dff4212ef87db
Justin Clark-Casey <justincc@justincc.org>
2018-08-24 18:22:07 +0100

    factor out code into add_genes(), add_organisms(), add_relationships()

--- slurp.py
+++ slurp.py
@@ -4,6 +4,21 @@ import neo4j.v1
 import psycopg2.extras
 
 
+def add_genes(_genes):
+    for im_id, gene in _genes.items():
+        print(gene)
+        session.run("CREATE (:gene { im_id:'%s', id:'%s', type:'%s' })" % (im_id, gene['external_primary_id'], gene['type']))
+
+def add_organisms(_organisms):
+    for im_id, organism in _organisms.items():
+        print(organism)
+        session.run("CREATE (:organism { im_id:'%s', id:'%s', name: '%s', type:'%s' })" % (im_id, organism['external_primary_id'], organism['name'], organism['type']))
+
+def add_relationships(_genes):
+    for im_id, gene in _genes.items():
+        session.run("MATCH (g:gene {im_id:'%s'}), (o:organism {im_id:'%s'}) CREATE (g)-[:IN_GENOME_OF]->(o)" % (im_id, gene['internal_organism_id']))
+
+
 genes = {}
 organisms = {}
 
@@ -21,20 +36,14 @@ with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'pass
                     'type' : row['class']
                 }
 
-            for im_id, gene in genes.items():
-                print(gene)
-                session.run("CREATE (:gene { im_id:'%s', id:'%s', type:'%s' })" % (im_id, gene['external_primary_id'], gene['type']))
+            add_genes(genes)
 
             curs.execute("SELECT id, taxonid, name, class FROM organism")
 
             for row in curs:
                 organisms[row['id']] = { 'external_primary_id': row['taxonid'], 'name': row['name'], 'type': row['class'] }
 
-            for im_id, organism in organisms.items():
-                print(organism)
-                session.run("CREATE (:organism { im_id:'%s', id:'%s', name: '%s', type:'%s' })" % (im_id, organism['external_primary_id'], organism['name'], organism['type']))
-
-            for im_id, gene in genes.items():
-                session.run("MATCH (g:gene {im_id:'%s'}), (o:organism {im_id:'%s'}) CREATE (g)-[:IN_GENOME_OF]->(o)" % (im_id, gene['internal_organism_id']))
+            add_organisms(organisms)
+            add_relationships(genes)
 
 conn.close()
