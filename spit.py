00de0eac2eb7e8ebd3967b16475f82c4ea257401
Justin Clark-Casey <justincc@justincc.org>
2018-09-13 14:51:52 +0100

    pull out type of retrieved node

--- spit.py
+++ spit.py
@@ -5,4 +5,6 @@ import neo4j.v1
 with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'passw0rd')) as driver:
     with driver.session() as session:
         result = session.run("match (n {id:'b0005'}) return n")
-        print(result.single())
+        record = result.single()
+        node = record['n']
+        print(node['type'])
