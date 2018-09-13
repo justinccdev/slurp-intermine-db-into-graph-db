2d19de4438cbc97a7fecdb3e81ed39ebcc2e0d9a
Justin Clark-Casey <justincc@justincc.org>
2018-09-13 13:48:27 +0100

    refactor for pep8 compliance

--- slurp.py
+++ slurp.py
@@ -4,43 +4,52 @@ import neo4j.v1
 import psycopg2.extras
 
 
-def get_im_genes(curs):
-    curs.execute("SELECT id, primaryidentifier, organismid, class FROM gene LIMIT 5")
+def get_im_genes(_curs):
+    _curs.execute("SELECT id, primaryidentifier, organismid, class FROM gene LIMIT 5")
 
-    genes = {}
+    _genes = {}
 
-    for row in curs:
-        genes[row['id']] = {
+    for row in _curs:
+        _genes[row['id']] = {
             'external_primary_id': row['primaryidentifier'],
             'internal_organism_id': row['organismid'],
-            'type' : row['class']
+            'type': row['class']
         }
 
-    return genes
+    return _genes
 
-def get_im_organisms(curs):
-    curs.execute("SELECT id, taxonid, name, class FROM organism")
+
+def get_im_organisms(_curs):
+    _curs.execute("SELECT id, taxonid, name, class FROM organism")
 
     organisms = {}
 
-    for row in curs:
-        organisms[row['id']] = { 'external_primary_id': row['taxonid'], 'name': row['name'], 'type': row['class'] }
+    for row in _curs:
+        organisms[row['id']] = {'external_primary_id': row['taxonid'], 'name': row['name'], 'type': row['class']}
 
     return organisms
+
 
 def add_genes(_genes):
     for im_id, gene in _genes.items():
         print(gene)
-        session.run("CREATE (:gene { im_id:'%s', id:'%s', type:'%s' })" % (im_id, gene['external_primary_id'], gene['type']))
+        session.run(
+            "CREATE (:gene { im_id:'%s', id:'%s', type:'%s' })" % (im_id, gene['external_primary_id'], gene['type']))
+
 
 def add_organisms(_organisms):
     for im_id, organism in _organisms.items():
         print(organism)
-        session.run("CREATE (:organism { im_id:'%s', id:'%s', name: '%s', type:'%s' })" % (im_id, organism['external_primary_id'], organism['name'], organism['type']))
+        session.run(
+            "CREATE (:organism { im_id:'%s', id:'%s', name: '%s', type:'%s' })"
+            % (im_id, organism['external_primary_id'], organism['name'], organism['type']))
+
 
 def add_relationships(_genes):
     for im_id, gene in _genes.items():
-        session.run("MATCH (g:gene {im_id:'%s'}), (o:organism {im_id:'%s'}) CREATE (g)-[:IN_GENOME_OF]->(o)" % (im_id, gene['internal_organism_id']))
+        session.run(
+            "MATCH (g:gene {im_id:'%s'}), (o:organism {im_id:'%s'}) CREATE (g)-[:IN_GENOME_OF]->(o)"
+            % (im_id, gene['internal_organism_id']))
 
 
 conn = psycopg2.connect(dbname='synbiomine-v6', user='justincc', cursor_factory=psycopg2.extras.DictCursor)
