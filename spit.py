b3c2156d4537222454041d6c3f71bddd89d620a0
Justin Clark-Casey <justincc@justincc.org>
2018-09-17 14:58:11 +0100

    Read in genomic model and store terms for classes. Use this to generate rdf

--- spit.py
+++ spit.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
 
 import neo4j.v1
+from lxml import etree
+
+
+tree = etree.parse('intermine/genomic_model.xml')
+model_package = tree.xpath('//model/@package')
+
+terms_for_classes = {}
+
+for _class in tree.xpath('//class'):
+    attrib = _class.attrib
+
+    if 'term' in attrib:
+        terms_for_classes[model_package[0] + '.' + attrib['name']] = attrib['term']
+
+"""
+for k, v in terms_for_classes.items():
+    print(k, v)
+"""
 
 with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'passw0rd')) as driver:
     with driver.session() as session:
@@ -8,8 +26,10 @@ with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'pass
         record = result.single()
         node = record['n']
 
-        if node['type'] == 'org.intermine.model.bio.Gene':
-            _type = "sio:SIO_010035"
+        type = node['type']
+
+        if type in terms_for_classes:
+            _type = terms_for_classes[type]
         else:
             _type = None
 
