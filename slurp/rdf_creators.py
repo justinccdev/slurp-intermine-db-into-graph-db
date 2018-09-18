33139fa705351b98ca312c27d8fd1f316aab4a8b
Justin Clark-Casey <justincc@justincc.org>
2018-09-18 17:28:01 +0100

    hardcode node subject to 'ecogene'

--- slurp/rdf_creators.py
+++ slurp/rdf_creators.py
@@ -1,5 +1,5 @@
 def create_node_subject(_id):
-    return 'http://synbiomine.org/ncbi:%s' % _id
+    return 'http://synbiomine.org/ecogene:%s' % _id
 
 
 def find_rdf_prefix_if_available(term, extensions):
