9308db89631355c141a9d33d537da249a2473cdc
Justin Clark-Casey <justincc@justincc.org>
2018-09-19 14:52:40 +0100

    close psycopg2 connection with syntax rather than manually

--- slurp.py
+++ slurp.py
@@ -6,14 +6,11 @@ import slurp.adders
 import slurp.slurpers
 
 
-conn = psycopg2.connect(dbname='synbiomine-v6', user='justincc', cursor_factory=psycopg2.extras.DictCursor)
-
-with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'passw0rd')) as driver:
-    with driver.session() as session:
-        with conn.cursor() as curs:
-            genes = slurp.slurpers.get_im_genes(curs)
-            slurp.adders.add_genes(session, genes)
-            slurp.adders.add_organisms(session, slurp.slurpers.get_im_organisms(curs))
-            slurp.adders.add_relationships(session, genes)
-
-conn.close()
+with psycopg2.connect(dbname='synbiomine-v6', user='justincc', cursor_factory=psycopg2.extras.DictCursor) as conn:
+    with neo4j.v1.GraphDatabase.driver('bolt://localhost:7687', auth=('neo4j', 'passw0rd')) as driver:
+        with driver.session() as session:
+            with conn.cursor() as curs:
+                genes = slurp.slurpers.get_im_genes(curs)
+                slurp.adders.add_genes(session, genes)
+                slurp.adders.add_organisms(session, slurp.slurpers.get_im_organisms(curs))
+                slurp.adders.add_relationships(session, genes)
