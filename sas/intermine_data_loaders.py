53fd9126c55a0568864128a9d787da4a7ff6fce0
Justin Clark-Casey <justincc@justincc.org>
2018-09-21 14:34:20 +0100

    change get_im_genes() so that it will load all intermine db fields, with mapping where necessary

--- sas/intermine_data_loaders.py
+++ sas/intermine_data_loaders.py
@@ -1,19 +1,29 @@
 def get_im_genes(curs):
     curs.execute("SELECT * FROM gene LIMIT 5")
 
+    _map = {
+        # This is a hack because the primary identifier is not an accession number and the actual ncbigene ID is
+        # not captured by Synbiomine
+        'secondaryidentifier':'external_primary_id',
+        'organismid':'internal_organism_id',
+        'primaryidentifier':'name',
+        'class':'type',
+        'id':None
+    }
+
     _genes = {}
 
     for row in curs:
-        _genes[row['id']] = {
-            # This is a hack because the primary identifier is not an accession number and the actual ncbigene ID is
-            # not captured by Synbiomine
-            'external_primary_id': row['secondaryidentifier'],
-            'internal_organism_id': row['organismid'],
-            'name': row['primaryidentifier'],
-            'type': row['class'],
-            'symbol': row['symbol'],
-            'description': row['description']
-        }
+        gene = {}
+
+        for k, v in row.iteritems():
+            if k in map:
+                k = _map[k]
+
+            if k is not None:
+                gene[k] = v
+
+        _genes[row['id']] = gene
 
     return _genes
 
