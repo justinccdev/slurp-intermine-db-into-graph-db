7b1424d243b064d338cf13b8948883157a2fefd4
Justin Clark-Casey <justincc@justincc.org>
2018-10-02 15:22:29 +0100

    even shorter form of ternary idiom

--- slurp.py
+++ slurp.py
@@ -40,7 +40,7 @@ with \
             for row in curs:
                 restrictions['Gene'].add(str(row['id']))
 
-        depth = args.depth if args.depth is not None else 1
+        depth = args.depth if args.depth else 1
 
         for i in range(depth):
             print('************ ROUND %d' % i)
